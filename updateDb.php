e1b716fdbe0f64b07fb05a25e9a14668ebdbb2ec
MarkCangila <mark.cangila@gmail.com>
2019-05-01 16:30:08 -0400

    added changes from server

--- updateDb.php
+++ updateDb.php
@@ -1,16 +1,16 @@
 <?php
 	$query = "DELTE FROM districts; DELETE FROM events;";
-	$user = root;
-	$password = $_ENV["databasePassword"];
+	$user = "pi";
+	$password = "Mandana2Goody";
 	$database = new mysqli("localhost", $user, $password, "scout_janssen");
 	if ($database->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
+		die("Connection failed: " . $database->connect_error);
 	}
 	$database->query($query);
 	$statusGet = curl_init("https://www.thebluealliance.com/api/v3/status");
 	$option = array('X-TBA-Auth-Key: nGj3OKIzPDXOqAs93AaNHKCZdrKcEXRpxGfl0nvzJyShGmEhVxZzjoHz86UGrolF');
 	curl_setopt_array(
-		$statusGet, 
+		$statusGet,
 		array(
 			CURLOPT_HTTPHEADER => $option,
 			CURLOPT_PROTOCOLS => CURLPROTO_HTTPS,
@@ -20,13 +20,12 @@
 	$tbaStatus = curl_exec($statusGet);
 	curl_close($statusGet);
 	$tbaStatus = json_decode($tbaStatus, true);
-	echo $tbaStatus;
 	$year = $tbaStatus["current_season"];
-	echo "{$year}";
-	$_ENV["currentYear"] = $year;
-	$districtsGet = curl_init("https://www.thebluealliance.com/api/v3/districts/{$year}");
+	$command = "echo 'year={$year}' | sudo tee -a /etc/environment && source /etc/environment";
+	shell_exec($command);
+	/*$districtsGet = curl_init("https://www.thebluealliance.com/api/v3/districts/{$year}");
 	curl_setopt_array(
-		$districtsGet, 
+		$districtsGet,
 		array(
 			CURLOPT_HTTPHEADER => $option,
 			CURLOPT_PROTOCOLS => CURLPROTO_HTTPS,
@@ -35,31 +34,32 @@
 	);
 	$districtList = curl_exec($districtsGet);
 	curl_close($districtsGet);
-	$districtList = json_decode($districtList, true);
-	echo $districtList;
-	foreach ($districtList as &$district) {
+	$district_List = json_decode($districtList, true);
+	foreach ($district_List as &$district) {
 		$query = "INSERT INTO districts (abbreviation, name, key, year) VALUES ('" . $district["abbreviation"] . "', '" . $district["display_name"] . "', '" . $district["key"] . "', '" . $district["year"] . "');";
 		$database->query($query);
 	}
-	unset($district);
+	unset($district);*/
 	$eventsGet = curl_init("https://www.thebluealliance.com/api/v3/events/{$year}/simple");
 	curl_setopt_array(
-		$eventsGet, 
+		$eventsGet,
 		array(
 			CURLOPT_HTTPHEADER => $option,
 			CURLOPT_PROTOCOLS => CURLPROTO_HTTPS,
 			CURLOPT_RETURNTRANSFER => true
 		)
 	);
-	$eventsList = $curl_exec($eventsGet);
+	$eventsList = curl_exec($eventsGet);
 	curl_close($eventsGet);
-	$eventsList = json_decode($events_list, true);
-	echo $eventsList;
-	foreach ($eventsList as &$event) {
+	$events = json_decode($eventsList, true);
+	echo var_dump($events);
+	foreach ($events as &$event) {
+		//echo var_dump($event);
 		if ($event["district"] === null) {
-			$query = "INSERT INTO events (key, name, start_date, end_date, year, district) VALUES ('" . $event["key"] . "', '" . $event["name"] . "', '" . $event["start_date"] . "', '" . $event["end_date"] . "', '" . $event["year"] . "', NULL);";
+			$query = "INSERT INTO events (code, name, start_date, end_date, year) VALUES ('" . $event["key"] . "', '" . $event["name"] . "', '" . $event["start_date"] . "', '" . $event["end_date"] . "', '" . $event["year"] . ");";
 		}
-		$query = "INSERT INTO events (key, name, start_date, end_date, year, district) VALUES ('" . $event["key"] . "', '" . $event["name"] . "', '" . $event["start_date"] . "', '" . $event["end_date"] . "', '" . $event["year"] . "', '" . $event["district"]["key"] . "');";
+		$query = "INSERT INTO events (code, name, start_date, end_date, year) VALUES ('" . $event["key"] . "', '" . $event["name"] . "', '" . $event["start_date"] . "', '" . $event["end_date"] . "', '" . $event["year"] . "');";
+		echo $query;
 		$database->query($query);
 	}
 	unset($event);
@@ -67,24 +67,30 @@
 	while (true) {
 		$teams = curl_init("https://www.thebluealliance.com/api/v3/teams/{$year}/{$page}/simple");
 		curl_setopt_array(
-			$eventsGet, 
+			$teams,
 			array(
 				CURLOPT_HTTPHEADER => $option,
-				CURLOPT_PROTOCOLS => CURLPROTO_HTTPS,
-				CURLOPT_RETURNTRANSFER => true
+                        	CURLOPT_PROTOCOLS => CURLPROTO_HTTPS,
+                        	CURLOPT_RETURNTRANSFER => true
 			)
 		);
-		$teamList = $curl_exec($teams);
+		$teamList = curl_exec($teams);
+		//echo var_dump(empty($teamList));
 		$httpCode = curl_getinfo($teams, CURLINFO_HTTP_CODE);
+		//if ($page > 15) echo var_dump($teamList);
 		curl_close($teams);
-		if ($httpCode === 404) {
+		//echo "{$page}";
+		$tempTeam = (array)$teamList;
+		if ($teamList == "[]") {
+			//echo var_dump($teamList);
 			break;
 		}
-		$teamList = json_decode($teamList);
+		$teamList = json_decode($teamList, true);
 		echo $teamList;
 		foreach ($teamList as &$team) {
-			$query = "INSERT INTO teams (key, number, name) VALUES ('" . $team["key"] . "', '" . $team["team_number"] . "', '" . $team["nickname"] . "');";
-			$databse->query($query);
+			$query = "INSERT INTO teams (code, number, name) VALUES ('" . $team["key"] . "', '" . $team["team_number"] . "', '" . $team["nickname"] . "');";
+			$database->query($query);
 		}
+		$page = $page + 1;
 	}
 ?>
