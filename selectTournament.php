e09b6e47e57d71d438a12bf54a7b73d4c192f059
MarkCangila <mark.cangila@gmail.com>
2019-05-02 17:55:24 -0400

    Added tournament select form

--- selectTournament.php
+++ selectTournament.php
@@ -1,10 +1,23 @@
 <html>
 	<body>
 		<h1>Select tournament below</h1>
-			<form action="/loadTournament.php" method="post">
-				<select>
-					<?php 
-						$query = "SELECT code, name FROM events;"
-						$password = $_ENV["HTTP_databsePassword"];
-						$user = "pi";
-						$databse = new mysqli("localhost", $user, $password, "scout_janssen");
+		<form action="/loadTournament.php" method="post">
+			<select name="eventChoice">
+				<?php 
+					$query = "SELECT code, name FROM events;"
+					$password = getenv("DBPASS");
+					$user = "pi";
+					$databse = new mysqli("localhost", $user, $password, "scout_janssen");
+					$events = $database->query($query);
+					if ($events->num_rows > 0) {
+						while($event = $result->fetch_assoc()) {
+							echo '<option value="' . $event["code"] . '">' . $event["name"] . '</option>';
+						}
+					}
+					?>
+			</select>
+				
+			<input type="submit" value="Submit" />
+		</form>
+	</body>
+</html>
