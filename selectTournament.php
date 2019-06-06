0470d33ff2e1add537641a70647cbb3954da0085
Keon Sanavandi <keonsanavandijoon@gmail.com>
2019-06-06 01:53:20 +0100

    Fixed update TOurn

--- selectTournament.php
+++ selectTournament.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html>
 	<body>
 		<h1>Select tournament below</h1>
@@ -27,33 +26,3 @@
 		</form>
 	</body>
 </html>
-=======
-<html>
-	<body>
-		<h1>Select tournament below</h1>
-		<form action="/loadTournament.php" method="post">
-			<select name="eventChoice">
-				<?php
-					$query = "SELECT code, name FROM events ORDER BY name;";
-					$password = getenv("DBPASS");
-					$user = "pi";
-					$database = new mysqli("localhost", $user, $password, "scout_janssen");
-					if ($database->connect_error) {
-						echo '</select>';
-						die("unable to connect: " . $database->connect_error);
-					}
-					$events = $database->query($query);
-					$menu = "";
-					if ($events->num_rows > 0) {
-						while($event = $events->fetch_assoc()) {
-							$menu = $menu . '<option value="' . $event["code"] . '">' . $event["name"] . '</option>';
-						}
-					}
-					$menu = $menu . "</select>";
-					echo $menu;
-				?>
-			<input type="submit" value="Submit" />
-		</form>
-	</body>
-</html>
->>>>>>> fa88c7b473cba43e1c568d39dca8070964d7c5fc
