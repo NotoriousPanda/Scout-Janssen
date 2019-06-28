f2ce61caf414369d1ab13293838e07b8314d1ada
Keon Sanavandi <keonsanavandijoon@gmail.com>
2019-06-28 11:53:15 -0400

    You guessed it...

--- updateTournament.php
+++ updateTournament.php
@@ -14,6 +14,7 @@
 			$database->query($query);
 			$option = array('X-TBA-Auth-Key: nGj3OKIzPDXOqAs93AaNHKCZdrKcEXRpxGfl0nvzJyShGmEhVxZzjoHz86UGrolF');
 			echo($_POST["eventChoice"]);
+			echo("https://www.thebluealliance.com/api/v3/events/" . $_POST["eventChoice"] . "/teams/keys");
 			$teamsGet = curl_init("https://www.thebluealliance.com/api/v3/events/" . $_POST["eventChoice"] . "/teams/keys");
 			curl_setopt_array(
 				$teamsGet,
