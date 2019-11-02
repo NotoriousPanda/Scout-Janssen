d25124fd5483be33ef31eea28eba498eef41a03c
xHayden <haydencarp@outlook.com>
2019-11-02 16:37:46 -0400

    Not testing print

---
+++ js/mainPage.js
@@ -0,0 +1,87 @@
+let event_key = "2019gagr";
+let team_key = "";
+let teamData = ""
+var packet = [];
+async function go() {
+  //headers
+  const headers = new Headers();
+  headers.append('X-TBA-Auth-Key', 'qg4OFGslC8z4zpEdaR8qPA79OUCBCi6dpE1tWLDEZqHARJLhu1GL7s8Aqq84vvJP')
+  const init = {
+    method: 'GET',
+    headers: headers,
+  }
+  //Create request w/ url and function
+  async function rqAPI(url, func){
+    const request = new Request(url, init)
+    response = await fetch(request);
+    func();
+  }
+
+  rqAPI('https://www.thebluealliance.com/api/v3/event/' + event_key + "/matches", function() {
+    var matchNumbers = [];
+    var blueAllianceKeys = [];
+    var redAllianceKeys = [];
+    packet = [];
+    response.json().then(function(value){
+      teamData = value;
+      for(i=0; i < teamData.length; i++){
+        blueAllianceKeys = [];
+        redAllianceKeys = [];
+        packet.push([teamData[i].match_number, teamData[i].alliances.blue.team_keys, teamData[i].alliances.red.team_keys]);
+      }
+      packet.sort((a, b) => (a[0] > b[0]) ? 1 : -1);
+      for(i=0; i < packet.length; i++){ //for each element in packet
+        matchNumbers.push(packet[i][0]);
+        blueAllianceKeys.push(packet[i][1]);
+        redAllianceKeys.push(packet[i][2]);
+      }
+      for(i=0; i < teamData.length; i++){
+        createRow("match" + matchNumbers[i]);
+        let blueA = blueAllianceKeys[i];
+        let redA = redAllianceKeys[i];
+        for(var xxxtentacion = 0; xxxtentacion < blueA.length; xxxtentacion++){
+          blueA[xxxtentacion] = blueA[xxxtentacion].replace("frc", "");
+        }
+        for(var xxxtentacion = 0; xxxtentacion < redA.length; xxxtentacion++){
+          redA[xxxtentacion] = redA[xxxtentacion].replace("frc", "");
+        }
+
+        createColumn(matchNumbers[i], "match" + matchNumbers[i]);
+        createColumn(blueA[0] + ", " + blueA[1] + ", " + blueA[2], "match" + matchNumbers[i]);
+        createColumn(redA[0] + ", " + redA[1] + ", " + redA[2], "match" + matchNumbers[i]);
+      }
+
+    })
+    createRow("Matches");
+    createHead("Match:", "Matches");
+    createHead("Blue Teams:", "Matches");
+    createHead("Red Teams:", "Matches");
+    createHead("Scores:", "Matches");
+    createHead("Match Time:", "Matches");
+  });
+
+  rqAPI('https://www.thebluealliance.com/api/v3/event/' + event_key + '/teams/statuses', function() {
+    response.json().then(function(value){
+      teamData = value;
+    });
+  });
+
+
+  function createRow(id=""){
+    let tr = document.createElement("tr");
+    tr.appendChild(document.createTextNode(""));
+    document.getElementById("TBATable").appendChild(tr);
+    tr.id = id;
+  }
+  function createColumn(data, row, id=""){
+    let td = document.createElement("td");
+    td.appendChild(document.createTextNode(data));
+    document.getElementById(row).appendChild(td);
+  }
+  function createHead(data, row, id=""){
+    let th = document.createElement("th");
+    th.appendChild(document.createTextNode(data));
+    document.getElementById(row).appendChild(th);
+  }
+}
+go();
