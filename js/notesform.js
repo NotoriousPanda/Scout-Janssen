3d1c70dd883f5d982d0f57ef9d2775d243dcef23
xHayden <93haydcarp@csdecatur.net>
2019-11-04 19:30:38 -0500

    it still doesn't work, but this time, it crashes instead.

--- js/notesform.js
+++ js/notesform.js
@@ -1,12 +1,17 @@
+let timerCount = 0;
+
 let event_key = "2019gagr";
 let team_key = "";
 let teamData = ""
 var packet = [];
-var select = "qf"
+var select = "qm"
 var match = 0;
 let matchData;
 var team = 4026;
+var set = 1;
 let alliances = []
+let globalVal = []
+//Whenever typeOfMatch changes, a new array needs to be created and sorted with the setinterval using its values instead
 async function go() {
   //headers
   const headers = new Headers();
@@ -21,141 +26,109 @@ async function go() {
       response = await fetch(request);
     func();
   }
-  rqAPI('https://www.thebluealliance.com/api/v3/event/' + event_key + "/matches", function() {
-    response.json().then(function(value){
-        value = (checkSelect(value, select));
-        console.log(value)
-        value.sort((a, b) => (a.match_number > b.match_number) ? 1 : -1);
-            //Sorts out values not equal to type of match then sorts by number^
-        for (i = 0; i < value.length; i++) {
-            checkExist(value[i]); //function that makes matchdata exist
-            //document.getElementById("botpos").value = 
-        }
+    await rqAPI('https://www.thebluealliance.com/api/v3/event/' + event_key + '/matches', function() {
+        response.json().then(function (value) {
+            value = (checkSelect(value, select));
+            value.sort((a, b) => (a.match_number > b.match_number) ? 1 : -1);
+            globalVal = value;
+                //Sorts out values not equal to type of match then sorts by number^
+            setInterval(function () {
+                for (i = 0; i < value.length; i++) {
+                    changeForm(value[i]); //function that makes matchdata exist
+                }
+            }, 100);
     })
   });
 }
 go();
 
-function checkExist(val) {
-    setInterval(function () {
-        if (document.getElementsByName("matchNumber").length) {
-            clearInterval(checkExist);
-            document.getElementsByName("matchNumber")[0].addEventListener('input', function () {
-                if (match != this.value) {
-                }
-                match = this.value;
-                if (val.match_number == match) {
-                    matchData = val;
-                    alliances = matchData.alliances.red.team_keys.concat(matchData.alliances.blue.team_keys); //Concat. Blue then red.
-                    for (i = 0; i < alliances.length; i++) {
-                        alliances[i] = alliances[i].replace("frc", "");
-                    }
-                    for (w = 0; w < alliances.length; w++) {
-                        e = document.getElementById("team" + (w + 1));
-                        e.value = alliances[w];
-                        if (!e.hasChildNodes()) {
-                            e.appendChild(document.createTextNode(alliances[w]));
-                        }
-                        e.replaceChild(document.createTextNode(alliances[w]), e.childNodes[0]);
-                    }
-                    x = document.getElementById("teamNumber");
-                    team = x.options[x.selectedIndex].value;
-                    changeBotPos()
-                }
-            });
+function checkMatch() {
+    if (document.getElementsByName("matchNumber").length);
+    document.getElementsByName("matchNumber")[0].addEventListener('input', function () {
+        if (match < globalVal.length) {
+            match = this.value;
         }
-    }, 100);
-
-
-    setInterval(function () {
-        if (document.getElementsByName("teamNumber").length) {
-            clearInterval(checkExist);
-            document.getElementsByName("teamNumber")[0].addEventListener('input', function () {
-                if (team != this.value) {
-                }
-                team = this.value;
-                changeBotPos()
-            });
+        else {
+            
         }
-    }, 100);
+    });
+   
 }
+
+
+function checkTeam() {
+    if (document.getElementsByName("teamNumber").length) {
+        document.getElementsByName("teamNumber")[0].addEventListener('input', function () {
+            team = this.value;
+        });
+    }
+}
+
+function checkSet() {
+    if (document.getElementsByName("setNumber").length) {
+        document.getElementsByName("setNumber")[0].addEventListener('input', function () {
+            set = this.value;
+        });
+    }
+}
+
 function checkTypeOfMatch() {
-    setInterval(function () {
-        if (document.getElementsByName("compLevel").length) {
-            clearInterval(checkTypeOfMatch);
-            document.getElementsByName("compLevel")[0].addEventListener('input', function () {
-                select = this.value;
-                changeBotPos()
-            });
-        }
-    }, 100);
+    if (document.getElementsByName("compLevel").length) {
+        clearInterval(checkTypeOfMatch);
+        document.getElementsByName("compLevel")[0].addEventListener('input', function () {
+            select = this.value;
+            console.log(select)
+        });
+    }
 }
 
-/*setInterval(function () {
-    checkMatchNumber();
-}, 100)
+function changeForm(val) {
+    checkMatch();
+    checkTeam();
+    checkSet();
+    checkTypeOfMatch();
+    if (val.match_number == match && val.set_number == set) {
+        changeBotPos()
+        matchData = val;
+        alliances = matchData.alliances.red.team_keys.concat(matchData.alliances.blue.team_keys); //Concat. Blue then red.
+        for (i = 0; i < alliances.length; i++) {
+            alliances[i] = alliances[i].replace("frc", "");
+        }
+        for (w = 0; w < alliances.length; w++) {
+            e = document.getElementById("team" + (w + 1));
+            e.value = alliances[w];
+            if (!e.hasChildNodes()) {
+                e.appendChild(document.createTextNode(alliances[w]));
+            }
+            e.replaceChild(document.createTextNode(alliances[w]), e.childNodes[0]);
+        }
+        x = document.getElementById("teamNumber");
+        y = document.getElementById("setNumber");
+        team = x.options[x.selectedIndex].value;
+        set = y.value;
+        changeBotPos()
+    }
+}
 
-function checkMatchNumber() {
-    if (document.getElementById("matchNumber") = )
-}*/
 
 function changeBotPos() {
-    console.log(alliances[1])
     for (i = 0; i < alliances.length; i++) {
-        //console.log(team)
         if (alliances[i] == team) {
             var botpos = document.getElementById("botpos");
             botpos.value = i + 1;
-            console.log(alliances)
-            //botpos.setAttribute("value", i + 1);
         }
     }
 }
-//select = document.getElementsByName("compLevel").options[document.getElementsByName("compLevel").selectedIndex].value;
 
-/*function checkSelect(value, select) {
-    for (h = 0; h < value.length; h++) {
-        var counter = 0;
-        console.log(value)
-        if ((value[h].comp_level != select)) {
-            value.splice(h, 1); //splicing out data that isnt select
-            if (value[h].comp_level != select) {
-                checkSelect(value, select);
-            }
-        }
-        else {
-        }
-        for (x = 0; x < value.length; x++) {
-            if (value[x].comp_level != select) {
-                break;
-            }
-            else {
-                counter++;
-            }
-            if (counter == value.length) { //check if # of values is equal to # of values that are of type select
-                return value;
-            }
-        }
-    }
-}*/
+
 function checkSelect(value, select) {
-    var counter = 0;
+    var newArr = []
     for (i = 0; i < value.length; i++) {
         if (select == value[i].comp_level) {
-            counter++;
+            newArr.push(value[i])
         }
-        else {
-            value.splice(i, 1);
-            checkSelect(value, select);
+        if (i === value.length - 1) {
+            return newArr;
         }
-    }
-    if (counter == value.length) {
-        //console.log(value)
-        return value;
-    }
-
-    else {
-        checkSelect(value, select);
-        changeBotPos()
     }
 }
